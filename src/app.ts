24915eb9e37d801c186ca6bc57a915a71dd94a2f
ibrahim <ibrahiminfo8@gmail.com>
2025-05-16 20:31:08 +0530

    completed the setup of ts and node

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('hi');
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+
+app.listen(PORT,()=>{
+      console.log(`Server is running on http://localhost:${PORT}`);
+
+})
