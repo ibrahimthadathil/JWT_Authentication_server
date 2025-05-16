18054ca33eec9f21df919de7378809007e21fb41
ibrahim <ibrahiminfo8@gmail.com>
2025-05-16 22:15:26 +0530

    created server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT 
 
-
+  
 app.listen(PORT,()=>{
       console.log(`Server is running on http://localhost:${PORT}`);
 
-})
+}) 
