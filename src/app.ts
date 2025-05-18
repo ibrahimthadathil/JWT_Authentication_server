625d08c32491959d0a170c80319f2260bc460ca7
ibrahim <ibrahiminfo8@gmail.com>
2025-05-18 23:25:14 +0530

    complete cors integration testing stage

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
-const app = express()
-const PORT = process.env.PORT 
 
-  
+const target = {
+      origin: process.env.CLIENT_URL,
+      changeOrigin:true,
+      Credentials:true
+}
+const app = express()
+ 
+app.use(cors(target))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post('/test/:id',(req,res)=>{
+try {
+      console.log(req.params.id)
+ 
+} catch (error) {
+      console.log(error);
+      
+}})
+
+const PORT = process.env.PORT || 3000  
 app.listen(PORT,()=>{
       console.log(`Server is running on http://localhost:${PORT}`);
 
